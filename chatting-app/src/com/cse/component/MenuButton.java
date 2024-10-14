0a5e788f4caad267784d83fb91bebbb1ca3a77dc
jakir-md <mdjakir.cse.ju@gmail.com>
2024-10-14 17:35:15 +0600

    all icons added with dragable window

---
+++ chatting-app/src/com/cse/component/MenuButton.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.cse.component;
+
+import javax.swing.JButton;
+
+/**
+ *
+ * @author Jakir
+ */
+public class MenuButton extends JButton{
+    public MenuButton(){
+        setContentAreaFilled(false);
+    }
+}
